75e8310b5e7b12c1d3b2779201e430621eb85859
mrtechnostart <rambpandey238@gmail.com>
2023-07-24 19:17:41 +0530

    Remove Hardcoded Data

--- models/model.js
+++ models/model.js
@@ -1,10 +1,11 @@
+require("dotenv").config()
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  user: "mrtechnostart",
-  password: "rambpandey238@A",
-  database: "ram",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   connectionLimit: 10,
 });
 
